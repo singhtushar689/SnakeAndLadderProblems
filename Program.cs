a8b0279d112df0e3772e1f759b99b9daf429c5bd
singhtushar689 <singhtushar689@gmail.com>
2022-11-14 00:24:20 +0530

    Option in Snake & Ladder

--- Program.cs
+++ Program.cs
@@ -4,14 +4,13 @@
     {
         public static void Main(string[] args)
         {
-<<<<<<< HEAD
+
             Random random = new Random();  
             int RollingDice = random.Next(1, 7);
             Console.WriteLine(RollingDice);
-=======
             int PlayerPosition = 0;
+            PlayerPosition += RollingDice;
             Console.WriteLine("Player is at " + PlayerPosition + " position ");
->>>>>>> UC1SnakeLadder
         }
     }
 }
